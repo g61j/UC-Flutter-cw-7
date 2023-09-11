3112a8e50393a58819d7796dbf1cdae56f72c302
g61j <142681300+g61j@users.noreply.github.com>
2023-09-11 18:11:24 +0300

    Create MAin 7.dart

---
+++ MAin 7.dart
@@ -0,0 +1,21 @@
+import 'package:day7/Screens/home_screen.dart';
+import 'package:flutter/material.dart';
+
+// Screens
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Home(),
+    );
+  }
+}
